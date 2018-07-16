7b9aa19813f4c71b1f0b6f857d86b70a5ceeab4f
Skyler Daché <skyberskyman@gmail.com>
2018-07-15 22:10:09 -0400

    made ninja image change with direction

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+//Config variables:
+let stepSize = 20;
+let screen_width = 500;
+let screen_height = 500;
+
+//Ninja state variables:
 var leftValue = 450;
 var topValue = 100;
-var stepSize = 20;
-document.onkeydown = function(e) {
-    //console.log(e);
-    let screen_width = 500;
-    let screen_height = 500;
+var direction = "down";
+var imagenumber = "1";
+function change_variables(e) {
     var ninjastyle = document.getElementById("character").style;
+
     switch(e.keyCode) {
         case 37: //Left
+            direction = "left";
             if (leftValue >= stepSize) {
                 leftValue -= stepSize
             } else {
@@ -15,6 +21,7 @@ document.onkeydown = function(e) {
             }
             break;
         case 39: //Right
+            direction = "right";
             if (leftValue + stepSize + ninjastyle.width <= screen_width) {
                 leftValue += stepSize;
             } else {
@@ -22,6 +29,7 @@ document.onkeydown = function(e) {
             }
             break;
         case 38: //Up
+            direction = "top";
             if (topValue >= stepSize) {
                 topValue -= stepSize;
             } else {
@@ -29,6 +37,7 @@ document.onkeydown = function(e) {
             }
             break;
         case 40: //Down
+            direction = "down";
             if (topValue + stepSize + ninjastyle.height <= screen_height) {
                 topValue += stepSize;
             } else {
@@ -36,10 +45,16 @@ document.onkeydown = function(e) {
             }
             break;
         }
-    document.getElementById("character").style.left = leftValue + "px";
-    document.getElementById("character").style.top = topValue + "px";
-    console.log("leftValue = " +leftValue);
-    console.log("topValue = "+topValue);
-    console.log("screen width: "+screen_width);
-    console.log("screen height: "+screen_height)
+}
+function update_ninja() {
+    var ninjastyle = document.getElementById("character").style;
+    if (imagenumber == "1") {imagenumber = "2"}
+    else if (imagenumber == "2") {imagenumber = "1"} 
+    ninjastyle.left = leftValue + "px";
+    ninjastyle.top = topValue + "px";
+    ninjastyle.backgroundImage = "url('img/"+direction+imagenumber+".png')";
+}
+document.onkeydown = function(e) {
+    change_variables(e);
+    update_ninja();
 }
